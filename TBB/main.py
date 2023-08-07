90c640e13d7d54fb820f9380653cbdb13f0f452f
anamitrajoshi <anamitra.joshi@gmail.com>
2023-08-07 23:42:56 +0530

    modified rules page background

--- TBB/main.py
+++ TBB/main.py
@@ -12,7 +12,7 @@ SCREEN = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Menu")
 
 BG = pygame.image.load("assets/tbb.jpg")
-RULES = pygame.image.load(os.path.join("Assets", "rules.jpg"))
+RULES = pygame.image.load(os.path.join("Assets", "rules.png"))
 def get_font(size): 
     return pygame.font.Font("assets/font.ttf", size)
 
